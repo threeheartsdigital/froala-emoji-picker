b7e34679af8166b450c918b34b041f45d1c938ac
Ben Scutt <ben@emailoctopus.com>
2018-09-06 11:29:38 +0100

    Treat jQuery as an external resource.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,10 @@
 const mix = require('laravel-mix');
 
+mix.webpackConfig({
+    externals: {
+        jquery: 'jQuery'
+    }
+});
+
 mix.js('src/js/emojis.js', 'build/emojis.min.js')
     .sass('src/scss/emojis.scss', 'build/emojis.min.css');
