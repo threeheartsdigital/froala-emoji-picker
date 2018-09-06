8499eca1c20b400a95328e98a116b4d8855a88c9
Ben Scutt <ben@emailoctopus.com>
2018-09-06 11:06:00 +0100

    Match Froala's plugin initialisation logic, to ensure WebPack compatibility.

--- src/js/emojis.js
+++ src/js/emojis.js
@@ -3,7 +3,33 @@
  * License MIT
  * Copyright 2018 Three Hearts Digital
  */
-$(function () {
+
+(function (factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['jquery'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node/CommonJS
+        module.exports = function (root, jQuery) {
+            if (jQuery === undefined) {
+                // require('jQuery') returns a factory that requires window to
+                // build a jQuery instance, we normalize how we use modules
+                // that require this pattern but the window provided is a noop
+                // if it's defined (how jquery works)
+                if (typeof window !== 'undefined') {
+                    jQuery = require('jquery');
+                } else {
+                    jQuery = require('jquery')(root);
+                }
+            }
+
+            return factory(jQuery);
+        };
+    } else {
+        // Browser globals
+        factory(window.jQuery);
+    }
+}(function ($) {
     const EmojiData = require('./emoji-data.json');
 
     $.extend($.FroalaEditor.POPUP_TEMPLATES, {
@@ -234,4 +260,4 @@ $(function () {
             this.emojis.setSelectedCategory(category);
         }
     });
-});
+}));
